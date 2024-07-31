5ba37f8a1f5ed1db58027b49cebb136f4c0b7a59
Zhongqiang Huang <zhuang@fixie.ai>
2024-07-30 21:56:29 -0700

    Update Justfile

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 cd $HOME
 mkdir workspace
 cd workspace
-git clone git@github.com:fixie-ai/ultravox.git -b main
+# git clone git@github.com:fixie-ai/ultravox.git -b main
+git clone git@github.com:zqhuang211/ultravox.git -b zhuang.2024-07-15-ultravox.blsp-kd-4a
+
 cd ultravox
 mkdir -p ~/.local/bin
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
